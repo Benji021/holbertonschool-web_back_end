aef660f090d1209aeb8a2a0343030a59f7349395
Benji021 <9548holbertonstudents.com>
2025-03-31 10:45:52 +0200

    Test

--- ES6_data_manipulation/test.js
+++ ES6_data_manipulation/test.js
@@ -1,4 +1,4 @@
-const { getListStudents } = require("./0-get_list_students.js");
+const { getListStudents } = require("./1-get_list_student_ids.js");
 const { getListStudentIds } = require("./1-get_list_student_ids.js");
 
 console.log(getListStudents()); // Vérifie la liste des étudiants
