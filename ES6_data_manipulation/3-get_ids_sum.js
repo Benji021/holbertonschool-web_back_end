931f5d129275a02dd3d53552ede786a4fe777bc8
Benji021 <9548holbertonstudents.com>
2025-03-31 11:01:28 +0200

    Reduce

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -20,5 +20,12 @@ function getListStudents() {
     return students.filter(student => student.location === city);
   }
   
+  function getStudentsByLocation(students, city) {
+    if (!Array.isArray(students)) {
+      return [];
+    }
+    return students.reduce(student => student.location === city);
+    }
+    
     // Export the function
 module.exports = { getListStudents, getListStudentIds, getStudentsByLocation };  
