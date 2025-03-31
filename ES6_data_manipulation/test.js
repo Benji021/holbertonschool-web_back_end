dc53973057d550f882bc6f5e208b8176ad70ccd3
Benji021 <9548holbertonstudents.com>
2025-03-31 10:51:56 +0200

    Test

--- ES6_data_manipulation/test.js
+++ ES6_data_manipulation/test.js
@@ -1,6 +1,7 @@
 const { getListStudents } = require("./2-get_students_by_loc.js");
 const { getListStudentIds } = require("./2-get_students_by_loc.js");
-console.log(getStudentsByLocation(getListStudents(), "San Francisco"));
+const { getStudentsByLocation } = require("./2-get_students_by_loc.js");
 
-console.log(getListStudents()); // Vérifie la liste des étudiants
-console.log(getListStudentIds(getListStudents())); // Vérifie les IDs
+console.log(getListStudents()); // Checks the list of students
+console.log(getListStudentIds(getListStudents())); // Checks student IDs
+console.log(getStudentsByLocation(getListStudents(), "San Francisco")); // Checks students in San Francisco
