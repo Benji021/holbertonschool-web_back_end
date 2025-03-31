2bf4a61f9d8049a03f03dabbd000ac6961022fce
Benji021 <9548holbertonstudents.com>
2025-03-31 10:36:30 +0200

    More mapping

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,17 @@
+function getListStudents() {
+    return [
+      { id: 1, firstName: "Guillaume", location: "San Francisco" },
+      { id: 2, firstName: "James", location: "Columbia" },
+      { id: 5, firstName: "Serena", location: "San Francisco" }
+    ];
+  }
+
+function getListStudentIds(students) {
+    if (!Array.isArray(students)) {
+      return [];
+    }
+    return students.map(student => student.id);
+  }
+
+// Export the function
+module.exports = getListStudentIds;
