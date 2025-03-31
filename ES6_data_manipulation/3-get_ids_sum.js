9a0432d0e18a1d2d839c45b22c0c87558aa99fa7
Benji021 <9548holbertonstudents.com>
2025-03-31 10:56:35 +0200

    Reduce

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,24 @@
+function getListStudents() {
+    return [
+      { id: 1, firstName: "Guillaume", location: "San Francisco" },
+      { id: 2, firstName: "James", location: "Columbia" },
+      { id: 5, firstName: "Serena", location: "San Francisco" }
+    ];
+  }
+  
+  function getListStudentIds(students) {
+    if (!Array.isArray(students)) {
+      return [];
+    }
+    return students.map(student => student.id);
+  }
+  
+  function getStudentsByLocation(students, city) {
+    if (!Array.isArray(students)) {
+      return [];
+    }
+    return students.filter(student => student.location === city);
+  }
+  
+    // Export the function
+module.exports = { getListStudents, getListStudentIds, getStudentsByLocation };  
