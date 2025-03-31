b5d5dcc1e483c7cccfa2d2992fc211fd999d451b
Benji021 <9548holbertonstudents.com>
2025-03-31 11:05:15 +0200

    Test

--- ES6_data_manipulation/test.js
+++ ES6_data_manipulation/test.js
@@ -6,4 +6,4 @@ const { getStudentIdsSum } = require("./3-get_ids_sum.js");
 console.log(getListStudents()); // Checks the list of students
 console.log(getListStudentIds(getListStudents())); // Checks student IDs
 console.log(getStudentsByLocation(getListStudents(), "San Francisco")); // Checks students in San Francisco
-console.log(getStudentIdsSum(getListStudents(), "San Francisco")); // Checks the sum of student IDs in San Francisco
+console.log(getStudentIdsSum(getListStudents())); // Checks the sum of student IDs in San Francisco
