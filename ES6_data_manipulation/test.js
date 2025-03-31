6dc72523072d9b8359712485d6a0ecac76d2f90b
Benji021 <9548holbertonstudents.com>
2025-03-31 10:39:09 +0200

    Test

--- ES6_data_manipulation/test.js
+++ ES6_data_manipulation/test.js
@@ -1,4 +1,4 @@
-const getListStudents = require("./0-get_list_students.js");
+const getListStudents = require("./1-get_list_student_ids.js");
 
 console.log(getListStudents());
 console.log(getListStudentIds(getListStudents()));
