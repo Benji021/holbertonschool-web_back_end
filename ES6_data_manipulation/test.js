e83778060128ebd606e3890a373b325115a9351b
Benji021 <9548holbertonstudents.com>
2025-04-01 10:09:46 +0200

    Test

--- ES6_data_manipulation/test.js
+++ ES6_data_manipulation/test.js
@@ -1,6 +1,8 @@
-const { getListStudents, getListStudentIds, getStudentsByLocation, getStudentIdsSum } = require("./3-get_ids_sum.js");
+const { getListStudents, getListStudentIds, getStudentsByLocation, getStudentIdsSum, updateStudentGradeByCity, createInt8TypedArray } = require("./3-get_ids_sum.js");
 
 console.log(getListStudents()); // Checks the list of students
 console.log(getListStudentIds(getListStudents())); // Checks student IDs
 console.log(getStudentsByLocation(getListStudents())); // Checks students
 console.log(getStudentIdsSum(getListStudents())); // Checks the sum of student IDs
+console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 1, grade: 90 }, { studentId: 2, grade: 85 }])); // Checks the updated grades 
+console.log(createInt8TypedArray(10, 2, 42)); // Checks the creation of Int8TypedArray
