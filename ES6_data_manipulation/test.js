152b896218ad91b8cf75683e75f4c9be41d0c754
Benji021 <9548holbertonstudents.com>
2025-04-01 10:14:10 +0200

    Test

--- ES6_data_manipulation/test.js
+++ ES6_data_manipulation/test.js
@@ -1,4 +1,4 @@
-const { getListStudents, getListStudentIds, getStudentsByLocation, getStudentIdsSum, updateStudentGradeByCity, createInt8TypedArray, setFromArray } = require("./3-get_ids_sum.js");
+const { getListStudents, getListStudentIds, getStudentsByLocation, getStudentIdsSum, updateStudentGradeByCity, createInt8TypedArray, setFromArray, hasValueFromArray } = require("./3-get_ids_sum.js");
 
 console.log(getListStudents()); // Checks the list of students
 console.log(getListStudentIds(getListStudents())); // Checks student IDs
@@ -7,3 +7,4 @@ console.log(getStudentIdsSum(getListStudents())); // Checks the sum of student I
 console.log(updateStudentGradeByCity(getListStudents(), "San Francisco", [{ studentId: 1, grade: 90 }, { studentId: 2, grade: 85 }])); // Checks the updated grades 
 console.log(createInt8TypedArray(10, 2, 42)); // Checks the creation of Int8TypedArray
 console.log(setFromArray([1, 2, 3, 4, 5])); // Checks the creation of Set from array
+console.log(hasValueFromArray([1, 2, 3, 4, 5], 3)); // Checks if value is in array
