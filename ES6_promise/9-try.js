6609621efaf1cc8d57d2176180ff926c1db52c79
Benji021 <9548holbertonstudents.com>
2025-04-01 10:51:41 +0200

    Throw error / try catch

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,17 @@
+export default function guardrail(mathFunction) {
+    const queue = [];
+
+    try {
+        // Executing the mathFunction function and adding the result to the queue
+        const result = mathFunction();
+        queue.push(result);
+    } catch (error) {
+        // In the event of an error, add the error message to the queue
+        queue.push(error.message);
+    } finally {
+        // Regardless of whether an error occurred, add the message 'Guardrail was processed' to the queue
+        queue.push('Guardrail was processed');
+    }
+
+    return queue;
+}
