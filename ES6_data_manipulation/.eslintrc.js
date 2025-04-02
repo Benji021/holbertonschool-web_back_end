e32441748a3ca515d7d4d7d1ba768f833aef3bd1
Benji021 <9548holbertonstudents.com>
2025-04-02 16:09:00 +0200

    Eslintrc config

--- ES6_data_manipulation/.eslintrc.js
+++ ES6_data_manipulation/.eslintrc.js
@@ -1,18 +1,37 @@
-import js from "@eslint/js";
-import eslintPluginJest from "eslint-plugin-jest";
-import airbnbBase from "eslint-config-airbnb-base";
-
-export default [
-  js.configs.recommended,
-  airbnbBase,
-  {
-    plugins: { jest: eslintPluginJest },
-    rules: {
-      "max-classes-per-file": "off",
-      "no-underscore-dangle": "off",
-      "no-console": "off",
-      "no-shadow": "off",
-      "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"],
-    },
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
   },
-];
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'max-classes-per-file': 'off',
+    'no-underscore-dangle': 'off',
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
