e8de22cb1b022f3575bb48fe27d8cc31a628bfe7
Benji021 <9548holbertonstudents.com>
2025-04-02 16:42:59 +0200

    Reduce

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,36 +1,36 @@
 function getListStudents() {
-    return [
-      { id: 1, firstName: "Guillaume", location: "San Francisco" },
-      { id: 2, firstName: "James", location: "Columbia" },
-      { id: 5, firstName: "Serena", location: "San Francisco" }
-    ];
-  }
-  
-  function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-    return students.map(student => student.id);
-  }
-  
-  function getStudentsByLocation(students, city) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-    return students.filter(student => student.location === city);
-  }
-  
-  function getStudentIdsSum(students, city) {
-    if (!Array.isArray(students)) {
-      return 0;
-    }
-    // Filter students by location if city is provided
-    if (city) {
-        students = students.filter(student => student.location === city);
-    }    
-    // Add the IDs of the students
-    return students.reduce((sum,student) => sum + student.id, 0);
-    }
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
+}
 
-    // Export the function
-module.exports = { getListStudents, getListStudentIds, getStudentsByLocation, getStudentIdsSum };  
+function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.map(student => student.id);
+}
+
+function getStudentsByLocation(students, city) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+  return students.filter(student => student.location === city);
+}
+
+function getStudentIdsSum(students, city) {
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+  // Filter students by location if city is provided
+  if (city) {
+    students = students.filter(student => student.location === city);
+  }    
+  // Add the IDs of the students
+  return students.reduce((sum, student) => sum + student.id, 0);
+}
+
+// Export the function
+module.exports = { getListStudents, getListStudentIds, getStudentsByLocation, getStudentIdsSum };
