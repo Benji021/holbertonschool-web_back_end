daee882865e3df805f5ed234ea6aab4d5a8a9443
Benji021 <9548holbertonstudents.com>
2025-04-03 11:04:45 +0200

    Reduce

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,38 +1,7 @@
-function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-}
-
-function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  return students.map((student) => student.id);
-}
-
-function getStudentsByLocation(students, city) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  return students.filter((student) => student.location === city);
-}
-
-export default function getStudentIdsSum(students, city) {
+export default function getStudentIdsSum(students) {
   if (!Array.isArray(students)) {
     return 0;
   }
 
-  // Filtrer les étudiants par ville si une ville est fournie
-  const filteredStudents = city
-    ? students.filter((student) => student.location === city)
-    : students;
-
-  // Ajouter les IDs des étudiants
-  return filteredStudents.reduce(
-    (sum, student) => sum + student.id,
-    0,
-  );
+  return students.reduce((sum, student) => sum + student.id, 0);
 }
