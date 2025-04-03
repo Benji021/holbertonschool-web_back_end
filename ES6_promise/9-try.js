7cb52eb783348e97e5a44dc496fae793a4aa275f
Benji021 <9548holbertonstudents.com>
2025-04-03 16:16:23 +0200

    Throw an error

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,17 +1,6 @@
-export default function guardrail(mathFunction) {
-  const queue = [];
-
-  try {
-    // Executing the mathFunction function and adding the result to the queue
-    const result = mathFunction();
-    queue.push(result);
-  } catch (error) {
-    // In the event of an error, add the error message to the queue
-    queue.push(error.message);
-  } finally {
-    // Add the message 'Guardrail was processed' to the queue
-    queue.push('Guardrail was processed');
+export default function divideFunction(numerator, denominator) {
+    if (denominator === 0) {
+      throw new Error('cannot divide by 0');
+    }
+    return numerator / denominator;
   }
-
-  return queue;
-}
