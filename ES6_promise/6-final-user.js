c6e4ba837f0883fe79c746a57ae99d69db2a5884
Benji021 <9548holbertonstudents.com>
2025-04-03 11:53:47 +0200

    Handle multiple promises

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,16 +1,13 @@
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-    const signUpPromise = signUpUser(firstName, lastName);
-    const uploadPhotoPromise = uploadPhoto(fileName);
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
 
-    // Utiliser Promise.allSettled pour attendre que les deux promesses soient réglées
-    return Promise.allSettled([signUpPromise, uploadPhotoPromise])
-        .then(results => {
-            return results.map(result => ({
-                status: result.status,
-                value: result.status === 'fulfilled' ? result.value : result.reason
-            }));
-        });
+  return Promise.allSettled([userPromise, photoPromise])
+    .then((results) => results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : String(result.reason),
+    })));
 }
