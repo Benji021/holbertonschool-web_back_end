3c8b2839a738caf555ad4ea59feac9f9cd0f481d
Benji021 <9548holbertonstudents.com>
2025-04-03 11:10:31 +0200

    Map data structure

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,97 +1,11 @@
-function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-}
-
-function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  return students.map((student) => student.id);
-}
-
-function getStudentsByLocation(students, city) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  return students.filter((student) => student.location === city);
-}
-
-function getStudentIdsSum(students, city) {
-  if (!Array.isArray(students)) {
-    return 0;
-  }
-  let filteredStudents = students;
-  if (city) {
-    filteredStudents = students.filter((student) => student.location === city);
-  }
-  return filteredStudents.reduce((sum, student) => sum + student.id, 0);
-}
-
-function updateStudentGradeByCity(students, city, newGrades) {
-  return students
-    .filter((student) => student.location === city)
-    .map((student) => {
-      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
-      return {
-        ...student,
-        grade: gradeObj ? gradeObj.grade : 'N/A',
-      };
-    });
-}
-
-function createInt8TypedArray(length, position, value) {
-  if (position < 0 || position >= length) {
-    throw new Error('Position outside range');
-  }
-
-  const buffer = new ArrayBuffer(length);
-  const int8View = new Int8Array(buffer);
-  int8View[position] = value;
-
-  return buffer;
-}
-
-function setFromArray(arr) {
-  return new Set(arr);
-}
-
-function hasValuesFromArray(set, arr) {
-  return arr.every((element) => set.has(element));
-}
-
-function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') return '';
-
-  return Array.from(set)
-    .filter((value) => typeof value === 'string' && value.startsWith(startString))
-    .map((value) => value.slice(startString.length))
-    .join('-');
-}
-
-function groceriesList() {
-  return new Map([
+export default function groceriesList() {
+  const groceries = [
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
-  ]);
-}
+  ];
 
-// Example usage:
-module.exports = {
-  getListStudents,
-  getListStudentIds,
-  getStudentsByLocation,
-  getStudentIdsSum,
-  updateStudentGradeByCity,
-  createInt8TypedArray,
-  setFromArray,
-  hasValuesFromArray,
-  cleanSet,
-  groceriesList,
-};
+  return new Map(groceries);
+}
