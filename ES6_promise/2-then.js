4f5f127c257b3dcb44a8b0f4fa6070031d055f4b
Benji021 <9548holbertonstudents.com>
2025-04-03 11:49:51 +0200

    Catch me if you can!

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,21 +1,11 @@
-function handleResponseFromAPI(promise) {
+export default function handleResponseFromAPI(promise) {
     return promise
-        .then(() => {
-            console.log("Got a response from the API");
-            return { status: 200, body: "success" };
-        })
-        .catch(() => {
-            console.log("Got a response from the API");
-            return new Error();
-        });
-}
-
-// Test avec une promesse résolue
-handleResponseFromAPI(Promise.resolve())
-    .then(response => console.log(response)) // { status: 200, body: 'success' }
-    .catch(error => console.error(error));
-
-// Test avec une promesse rejetée
-handleResponseFromAPI(Promise.reject())
-    .then(response => console.log(response))
-    .catch(error => console.error(error)); // Error {}
+      .then(() => {
+        console.log('Got a response from the API');
+        return { status: 200, body: 'success' };
+      })
+      .catch(() => {
+        console.log('Got a response from the API');
+        return new Error();
+      });
+  }
