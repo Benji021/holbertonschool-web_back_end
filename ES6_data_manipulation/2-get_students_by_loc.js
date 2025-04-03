36ea9ccb18b4db4e133d12e91eaf34e6306e33cf
Benji021 <9548holbertonstudents.com>
2025-04-03 11:04:00 +0200

    Filter

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,21 +1,7 @@
-function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-}
-
-function getListStudentIds(students) {
-  if (!Array.isArray(students)) {
-    return [];
-  }
-  return students.map((student) => student.id);
-}
-
 export default function getStudentsByLocation(students, city) {
   if (!Array.isArray(students)) {
     return [];
   }
+
   return students.filter((student) => student.location === city);
 }
