f787746289cb82b483ecda4c0c8ec4acf577149b
Benji021 <9548holbertonstudents.com>
2025-04-03 10:20:21 +0200

    Keep every promise you make and only make promises you can keep

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,13 +1,5 @@
-function getResponseFromAPI() {
+export default function getResponseFromAPI() {
     return new Promise((resolve, reject) => {
-        // Simule une requête API
-        setTimeout(() => {
-            const success = true; // Replace with a real condition if necessary
-            if (success) {
-                resolve("Success: Data received from API");
-            } else {
-                reject(new Error("Failed to fetch data from API"));
-            }
-        }, 1000); // Simulate a 1 second delay
+        resolve('Success');
     });
 }
