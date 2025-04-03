7ac44c57b6d25092ffbf4238503cfb8d671d0720
Benji021 <9548holbertonstudents.com>
2025-04-03 15:29:55 +0200

    Keep every promise you make and only make promises you can keep

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,10 +1,10 @@
 export default function getResponseFromAPI() {
-    return new Promise((resolve, reject) => {
-      const success = true;
-      if (success) {
-        resolve('Success');
-      } else {
-        reject(new Error('Fail'));
-      }
-    });
-  }
+  return new Promise((resolve, reject) => {
+    const success = true;
+    if (success) {
+     resolve('Success');
+    } else {
+      reject(new Error('Fail'));
+    }
+  });
+}
