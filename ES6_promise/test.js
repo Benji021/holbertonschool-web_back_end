e39f1612ce785f0a3b33ec4561f27dc9dd622a0b
Benji021 <9548holbertonstudents.com>
2025-04-03 10:32:29 +0200

    Test

--- ES6_promise/test.js
+++ ES6_promise/test.js
@@ -4,8 +4,9 @@ test('getResponseFromAPI renvoie "Success"', async () => {
     await expect(getResponseFromAPI()).resolves.toBe('Success');
 });
 
-import { getFullResponseFromAPI } from './1-promise'; // Si c'est dans un autre fichier
-
 test('getFullResponseFromAPI renvoie un objet avec statut et corps', async () => {
-    await expect(getFullResponseFromAPI()).resolves.toEqual({ status: 200, body: 'Success' });
+    await expect(getFullResponseFromAPI(true)).resolves.toEqual({
+        status: 200,
+        body: 'Success'
+    });
 });
