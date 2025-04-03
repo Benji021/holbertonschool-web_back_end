da99e2d92481cd7f4bec19e4d8821e5c39649405
Benji021 <9548holbertonstudents.com>
2025-04-03 15:51:50 +0200

    Catch me if you can!

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -1,11 +1,9 @@
-export default function handleResponseFromAPI(promise) {
-  return promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => {
-      console.log('Got a response from the API');
-      return new Error();
-    });
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+   });
 }
