1aaf87387e34df8d362b4b15795ad7bd32417460
Benji021 <9548holbertonstudents.com>
2025-04-03 10:25:44 +0200

    Test

--- ES6_promise/test.js
+++ ES6_promise/test.js
@@ -3,3 +3,12 @@ import getResponseFromAPI from './0-promise';  // Assure-toi du bon chemin
 test('getResponseFromAPI renvoie "Success"', async () => {
     await expect(getResponseFromAPI()).resolves.toBe('Success');
 });
+
+// Tests 1-promise.js
+getFullResponseFromAPI(true)
+    .then(response => console.log(response))  // { status: 200, body: 'Success' }
+    .catch(error => console.error(error.message));
+
+getFullResponseFromAPI(false)
+    .then(response => console.log(response))
+    .catch(error => console.error(error.message)); // "The fake API is not working currently"
