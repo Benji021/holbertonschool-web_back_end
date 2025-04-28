a2d109071358adf505388041dba78f63ecd9c747
Benji021 <9548holbertonstudents.com>
2025-04-28 10:41:44 +0200

    Async Generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,4 +11,4 @@ async def async_generator():
     with 1 second interval."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.random() * 10
+        yield random.uniform(0, 10)
