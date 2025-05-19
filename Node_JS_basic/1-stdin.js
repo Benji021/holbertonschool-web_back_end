507ff4ff1519567db18ffd3c2b0dcfb5e4152e2f
Benji021 <9548holbertonstudents.com>
2025-05-19 11:06:14 +0200

    Using Process stdin

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -11,6 +11,7 @@ console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (input) => {
   console.log(`Your name is: ${input}`);
+  // You can close the input after the first entry, or wait longer.
   rl.close();
 });
 
