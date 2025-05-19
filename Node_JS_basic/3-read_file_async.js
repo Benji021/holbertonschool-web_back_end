6c651302f0faccb9d6874f11bb23203922956698
Benji021 <9548holbertonstudents.com>
2025-05-19 14:24:33 +0200

    Reading a file asynchronously with Node JS

--- Node_JS_basic/3-read_file_async.js
+++ Node_JS_basic/3-read_file_async.js
@@ -1,51 +1,40 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
+async function countStudents(path) {
+  try {
+    const data = await fs.promises.readFile(path, 'utf8');
+    const lines = data.split('\n').filter((line) => line.trim() !== ''); // Delete empty lines
+
+    if (lines.length === 0) throw new Error('Cannot load the database');
+
+    // Extract headers and rows of data
+    const headers = lines[0].split(',');
+    const students = lines.slice(1).map((line) => line.split(','));
+
+    if (students.length === 0) throw new Error('Cannot load the database');
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+
+    students.forEach((student) => {
+      const firstName = student[0].trim();
+      const field = student[3].trim(); // The field is assumed to be in 4th position
+
+      if (!fields[field]) {
+        fields[field] = [];
       }
-
-      const lines = data.split('\n').filter((line) => line.trim() !== ''); // Supprimer les lignes vides
-      if (lines.length === 0) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      // Extraire les en-têtes et les lignes de données
-      const headers = lines[0].split(',');
-      const students = lines.slice(1).map((line) => line.split(','));
-
-      if (students.length === 0) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      console.log(`Number of students: ${students.length}`);
-
-      const fields = {};
-
-      students.forEach((student) => {
-        const firstName = student[0].trim();
-        const field = student[3].trim(); // On suppose que le champ est en 4e position
-
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstName);
-      });
-
-      // Afficher les résultats pour chaque champ
-      for (const [field, names] of Object.entries(fields)) {
-        console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
-      }
-
-      resolve();
+      fields[field].push(firstName);
     });
-  });
+
+    // Display results for each field
+    for (const [field, names] of Object.entries(fields)) {
+      console.log(`Number of students in ${field}: ${names.length}. List: ${names.join(', ')}`);
+    }
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
