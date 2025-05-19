ad902be5911624a49b18676e8bd69969445e6281
Benji021 <9548holbertonstudents.com>
2025-05-19 15:21:11 +0200

    Write the Students controller

---
+++ Node_JS_basic/full_server/controllers/StudentsController.js
@@ -0,0 +1,37 @@
+import { readDatabase } from '../utils.js';
+
+class StudentsController {
+  static async getAllStudents(req, res) {
+    try {
+      const students = await readDatabase('/path/to/database.csv');
+      const sortedFields = Object.keys(students).sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
+      let output = 'This is the list of our students\n';
+
+      sortedFields.forEach((field) => {
+        const list = students[field].join(', ');
+        output += `Number of students in ${field}: ${students[field].length}. List: ${list}\n`;
+      });
+
+      res.status(200).send(output);
+    } catch (err) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+
+  static async getAllStudentsByMajor(req, res) {
+    const { major } = req.params;
+    if (!['CS', 'SWE'].includes(major)) {
+      return res.status(500).send('Major parameter must be CS or SWE');
+    }
+
+    try {
+      const students = await readDatabase('/path/to/database.csv');
+      const list = students[major] || [];
+      res.status(200).send(`List: ${list.join(', ')}`);
+    } catch (err) {
+      res.status(500).send('Cannot load the database');
+    }
+  }
+}
+
+export default StudentsController;
